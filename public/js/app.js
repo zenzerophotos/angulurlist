27e854fd8efde34d10134786c8f66600534a3554
Josh Pitman <shidogrey@gmail.com>
2016-03-02 20:18:27 -0500

    not workin yet

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,29 @@
 var app angular.module('ngTodo',[]);
 
 app.controller('listController',function ($scope){
-  $scope.todoList = [''];
+  $scope.todoList = ['add'];
 
 
-    $scope.saveTodo = function(){
-      var todoInfo = $scope.currentTodo;
-      $scope.todoList.push(todoInfo);
-      $scope.currentTodo = "";
-    }
+  $scope.saveTodo = function(){
+    var todoInfo = $scope.currentTodo;
+    $scope.todoList.push(todoInfo);
+    $scope.currentTodo = "";
+  }
 
-    $scope.removeTodo = function(singleTodo){
-      var position = $scope.todoList.indexOf(singleTodo);
-      $scope.todoList.splice(position, 1);
-    };
+  $scope.removeTodo = function(singleTodo){
+    var position = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice( position, 1);
+  }
+
+  $scope.editTodo = function(singleTodo){
+    var index = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice(index,1,$scope.currentTodo);
+    $scope.currentTodo = '';
+  }
+
+  $scope.moveUp = function(singleTodo){
+    var index = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice(index, 1);
+    $scope.todoList.splice(index-1, 0, singleTodo);
+  }
 });
