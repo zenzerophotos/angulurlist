5dffd0f6f60f73076f24693d2e3903eaaf5e5e23
Josh Pitman <shidogrey@gmail.com>
2016-03-05 14:52:11 -0500

    added image

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     logger = require('morgan')('dev'),
     server = express();
+      // io = require('socket.io').listen(server);
+      //
+    
 
 server.use(logger);
 server.use(express.static(__dirname+'/public'));
@@ -9,6 +12,11 @@ server.use(express.static(__dirname+'/public'));
 server.get('/', function(req,res){
   res.sendFile('public/html/index.html',{root: __dirname});
 });
+
+// io.sockets.on("connection",function(){
+//   console.log('working')
+// })
+
 
 server.listen(8080,function(){
   console.log('port 8080 running'+8080);
